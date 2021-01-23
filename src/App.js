3595ddcad61b1090f8af55ed8bfade05f2665dfb
Alina Tariq <alina.tariq@ryerson.ca>
2021-01-23 14:24:16 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './assets/css/app.min.css';
+import './assets/css/navbar.min.css';
+import './assets/css/main.min.css';
+import './assets/css/about.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './components/Navbar/NavigationBar';
 import Main from './components/main/Main';
