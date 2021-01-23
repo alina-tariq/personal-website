328ce05bbf69bf70e30a83f59f2ec2620e8fbee9
Alina Tariq <alina.tariq@ryerson.ca>
2021-01-23 06:48:59 -0500

    minor changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,9 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about">
-            <div className="text">
-                <h1>hello</h1>
-            </div>
+            <h1>hello</h1>
         </div>
     )
 }
