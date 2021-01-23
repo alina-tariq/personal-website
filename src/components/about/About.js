c8d0a8ea9292be8cfc31d05e730e8f676bffd2c6
Alina Tariq <alina.tariq@ryerson.ca>
2021-01-23 06:54:07 -0500

    minor changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Container,Row,Col} from '../../../node_modules/react-bootstrap';
+import MyPicture from '../../assets/img/meicon.png';
 
 const About = () => {
     return (
         <div>
             <Container fluid>
                 <Row>
-                    <Col className="about">
-                        <h1>hello there!</h1>
+                    <Col className="main">
+                        <img src={MyPicture} alt="mypic"/> 
+                        <br />
                     </Col>
                 </Row>
             </Container>
