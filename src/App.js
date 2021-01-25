cadf8567b79051d4ec6ceed76f6565e401a08e73
Alina Tariq <alina.tariq@ryerson.ca>
2021-01-25 10:49:35 -0500

    changes to website structure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './assets/css/app.min.css';
 import './assets/css/navbar.min.css';
 import './assets/css/main.min.css';
 import './assets/css/about.min.css';
+import './assets/css/resume.min.css';
+import './assets/css/projects.min.css';
+import './assets/css/contact.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './components/Navbar/NavigationBar';
 import Main from './components/main/Main';
 import About from './components/about/About';
-import Projects from './components/projects/Projects';
 import Resume from './components/resume/Resume';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
 
 //import {Container,Row,Col} from '../node_modules/react-bootstrap';
 
@@ -28,6 +32,9 @@ function App() {
       </section>
       <section>
         <Projects />
+      </section>
+      <section>
+        <Contact />
       </section>
     </>
   );
