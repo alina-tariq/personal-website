7388baa91cead8fedf2031451a28f53eaebe5ff8
Alina Tariq <alina.tariq@ryerson.ca>
2021-01-25 22:32:36 -0500

    resume changes

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Cat from '../../assets/img/pawcat.png';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Element } from 'react-scroll';
 
 const Resume = () => {
     return (
-        <div className="resume">
-            <div className="text">
-                <h1>hello</h1>
-            </div>
+        <div>
+            <Element id='/resume' name='/resume'>
+                <Container fluid>
+                    <Row>
+                        <Col className="resume">
+                            <div className="heading">
+                                <img src={Cat} alt="cat" id="heading-cat" />
+                                <h1 id="heading-text">Resume</h1>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Element>
         </div>
     )
 }
