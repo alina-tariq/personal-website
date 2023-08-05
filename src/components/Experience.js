ec02a3d81ba7172b1e3673d18f6b10a9809f4457
alina-tariq <alina.tariq@ryerson.ca>
2023-08-05 18:22:24 -0400

    refactored to make it easier to change parts without a lot of rewriting code

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,12 @@
 import { Element } from "react-scroll";
 
 export default function Experience() {
+    const logoImage = "absolute flex items-center justify-center rounded-full ring-4 ring-amber-500 lg:w-14 lg:h-14 lg:-left-7 w-12 h-12 -left-6";
+    const orgName = "mx-8 xl:text-xl lg:text-lg sm:text-base text-sm font-bold";
+    const title = "mx-8 xl:text-xl lg:text-lg sm:text-base text-sm";
+    const langDiv = "xl:text-base lg:text-sm text-xs pl-8 pt-2 flex flex-wrap";
+    const langCard = "bg-gradient-to-br from-regal-blue to-regal-purple font-bold mr-2 mb-2 px-2 py-1 rounded";
+
     return (
         <Element id='/experience' name='/experience'>
             <div className="h-auto bg-gray-900 md:pt-28 pt-24">
@@ -16,98 +22,84 @@ export default function Experience() {
                     <div className="md:w-3/5 flex flex-col items-center justify-center text-left
                     font-Merriweather md:pl-0 pl-10">
                         <ol class="relative border-l-4 border-amber-500 mt-2">
-                            <li class="lg:mt-10 md:mt-8 mt-5 mb-5 ml-6 text-white">
-                                <span class="absolute flex items-center justify-center rounded-full ring-4 ring-amber-500
-                                lg:w-14 lg:h-14 lg:-left-7 sm:w-12 sm:h-12 sm:-left-6 w-12 h-12 -left-6">
+                            <li className="lg:mt-10 md:mt-8 mt-5 mb-5 ml-6 text-white">
+                                <span className={logoImage}>
                                     <img
                                         className="rounded-full w-full"
                                         src={require("../assets/img/cognixion.jpeg")}
                                         alt="Cognixion Logo"
                                     />
                                 </span>
-                                <h3 class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm font-bold">Cognixion</h3>
-                                <p class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm">Software Developer</p>
-                                <div className="xl:text-base lg:text-sm text-xs pl-8 pt-2 flex flex-wrap">
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                <h3 className={orgName}>Cognixion</h3>
+                                <p className={title}>Software Developer</p>
+                                <div className={langDiv}>
+                                    <span className={langCard}>
                                         Java
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         Python
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         C/C++
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         C#
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         Django
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         Three.js
                                     </span>
                                 </div>
                             </li>
-                            <li class="mb-5 ml-6 text-white">
-                                <span class="absolute flex items-center justify-center rounded-full ring-4 ring-amber-500
-                                lg:w-14 lg:h-14 lg:-left-7 w-12 h-12 -left-6">
+                            <li className="mb-5 ml-6 text-white">
+                                <span className={logoImage}>
                                     <img
                                         className="rounded-full w-full"
                                         src={require("../assets/img/rcmp.jpeg")}
                                         alt="RCMP Logo"
                                     />
                                 </span>
-                                <h3 class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm font-bold">Royal Canadian Mounted Police</h3>
-                                <p class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm">Junior Programmer Analyst</p>
-                                <div className="xl:text-base lg:text-sm text-xs pl-8 pt-2 flex flex-wrap">
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple
-                                    font-bold mr-2 mb-2 px-2 py-1 rounded">
+                                <h3 className={orgName}>Royal Canadian Mounted Police</h3>
+                                <p className={title}>Junior Programmer Analyst</p>
+                                <div className={langDiv}>
+                                    <span className={langCard}>
                                         Java
                                     </span>
                                 </div>
                             </li>
-                            <li class="mb-5 ml-6 text-white">
-                                <span class="absolute flex items-center justify-center rounded-full ring-4 ring-amber-500
-                                lg:w-14 lg:h-14 lg:-left-7 sm:w-12 sm:h-12 sm:-left-6 w-12 h-12 -left-6">
+                            <li className="mb-5 ml-6 text-white">
+                                <span className={logoImage}>
                                     <img
                                         className="rounded-full w-full"
                                         src={require("../assets/img/cognixion.jpeg")}
                                         alt="Cognixion Logo"
                                     />
                                 </span>
-                                <h3 class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm font-bold">Cognixion</h3>
-                                <p class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm">Software Developer Intern</p>
-                                <div className="xl:text-base lg:text-sm text-xs pl-8 pt-2 flex flex-wrap">
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple  font-bold 
-                                    mr-2 mb-2 px-2 py-1 rounded">
+                                <h3 className={orgName}>Cognixion</h3>
+                                <p className={title}>Software Developer Intern</p>
+                                <div className={langDiv}>
+                                    <span className={langCard}>
                                         Python
                                     </span>
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple  font-bold 
-                                        mr-2 mb-2 px-2 py-1 rounded">
+                                    <span className={langCard}>
                                         C/C++
                                     </span>
                                 </div>
                             </li>
-                            <li class="xs:mb-5 mb-2 ml-6 text-white">
-                                <span class="absolute flex items-center justify-center rounded-full ring-4 ring-amber-500
-                                lg:w-14 lg:h-14 lg:-left-7 sm:w-12 sm:h-12 sm:-left-6 w-12 h-12 -left-6">
+                            <li className="xs:mb-5 mb-2 ml-6 text-white">
+                                <span className={logoImage}>
                                     <img
                                         className="rounded-full w-full"
                                         src={require("../assets/img/sickkids.jpeg")}
                                         alt="SickKids Logo"
                                     />
                                 </span>
-                                <h3 class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm font-bold">The Hospital for Sick Children</h3>
-                                <p class="mx-8 xl:text-xl lg:text-lg sm:text-base text-sm">Research Analyst</p>
-                                <div className="xl:text-base lg:text-sm text-xs pl-8 pt-2 flex flex-wrap">
-                                    <span class="bg-gradient-to-br from-regal-blue to-regal-purple font-bold 
-                                    mr-2 mb-2 px-2 py-1 rounded">
+                                <h3 className={orgName}>The Hospital for Sick Children</h3>
+                                <p className={title}>Research Analyst</p>
+                                <div className={langDiv}>
+                                    <span className={langCard}>
                                         MATLAB
                                     </span>
                                 </div>
