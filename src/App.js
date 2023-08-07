757c76898a1fdb7ebbc386a739f7ef063e1f10e2
alina-tariq <alina.tariq@ryerson.ca>
2023-08-07 18:04:00 -0400

    added footer and refactored skills component for readability

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
+import Footer from './components/Footer';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </main>
   );
 }
