475fcf82739bb4cce5ed2faf8ba850d4b3ee9141
alina-tariq <alina.tariq@ryerson.ca>
2023-08-09 14:45:36 -0400

    device detection to address spacing issue on iOS devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import { gsap } from "gsap";
 export default function Home() {
     const resetWindowScrollPosition = useCallback(() => window.scrollTo(0, 0), [])
     const fadeIn = useRef();
+    let greeting = "font-Sacramento lg:text-9xl xs:text-8xl xxs:text-7xl text-6xl font-[800] mx-auto text-transparent pt-5 md:pb-8 pb-4 xs:px-16 px-5 bg-clip-text bg-gradient-to-r from-orange-500 to-purple-500 md:drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)] sm:drop-shadow-[0_3px_3px_rgba(0,0,0,0.8)] drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]";
 
     // scrolls back to top on page refresh
     useEffect(() => {
@@ -20,17 +21,16 @@ export default function Home() {
         gsap.to(fadeIn.current, {opacity: 1, duration: 3, delay: 1.75});
     })
 
-    {console.log(window.navigator.userAgent)}
+    if (/iPad|iPhone|iPod/.test(window.navigator.userAgent)) {
+        greeting += " tracking-tighter";
+    }
 
     return (
         <div className="h-screen bg-gradient-to-b from-sky-700 to-gray-900 grid grid-cols-1 grid-flow-row 
             px-8 pb-8 sm:pt-32 pt-24">
             <div className="flex flex-col items-center justify-center text-center">
-                <div className="">
-                    <h1 className="font-Sacramento lg:text-9xl xs:text-8xl xxs:text-7xl text-6xl
-                    font-[800] mx-auto text-transparent pt-5 md:pb-8 pb-4 xs:px-16 px-5 bg-clip-text
-                    bg-gradient-to-r from-orange-500 to-purple-500 md:drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)] 
-                    sm:drop-shadow-[0_3px_3px_rgba(0,0,0,0.8)] drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">
+                <div>
+                    <h1 className={greeting}>
                         <TypeAnimation
                             sequence={[
                                 '',
