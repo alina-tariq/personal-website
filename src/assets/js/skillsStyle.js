d3819b7bb71e8f663bb9063fd51ed8413ff33669
alina-tariq <alina.tariq@ryerson.ca>
2023-08-09 15:52:29 -0400

    made badge text smaller

--- src/assets/js/skillsStyle.js
+++ src/assets/js/skillsStyle.js
@@ -6,7 +6,7 @@ const badgeSectionTitle = "text-amber-500 font-bold sm:text-xl text-lg pb-4";
 const badgeDiv = "flex flex-wrap justify-center text-center";
 const badgeImage = "w-20 h-auto flex flex-col items-center justify-top m-2";
 const badgeColour = "white";
-const badgeText = "pt-2 font-semibold";
+const badgeText = "sm:text-lg text-base pt-2 font-semibold";
 
 export {
     badgeSectionDiv,
