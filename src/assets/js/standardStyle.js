94b09a5c41898e37ce2af89b696cc70288bb394e
alina-tariq <alina.tariq@ryerson.ca>
2023-08-08 23:26:13 -0400

    final fix for spacing issue

--- src/assets/js/standardStyle.js
+++ src/assets/js/standardStyle.js
@@ -4,23 +4,27 @@
 const section = "h-auto bg-gray-900 md:pt-28 pt-24";
 const sectionTitleDiv = "md:w-2/5 w-full flex flex-col items-center justify-center text-center md:pb-0 pb-5";
 const sectionTitleUnderline = "text-stone-100 underline underline-offset-[10px]";
-const sectionTitle = "font-Sacramento xl:text-8xl sm:text-7xl text-6xl font-extrabold mx-auto text-transparent pt-2 pb-5 px-8 bg-clip-text bg-gradient-to-r from-orange-500 to-purple-500";
+let sectionTitle;
 
 const contentDiv = "md:w-3/5 w-full flex flex-col items-center justify-center font-Merriweather text-white";
 const content = "lg:text-lg sm:text-base text-sm mx-auto md:pt-2 md:px-16 sm:px-8 xxs:px-6 px-4";
 
 // fixes Sacramento font spacing issue on non-PC devices
-let isPC = true;
+let isPC;
 
-if (window.screen.orientation == 'landscape.primary' && window.screen.width > 1024) {
-    isPC = true;
-} else if (window.screen.orientation == 'potrait-primary' && window.screen.height > 1024) {
-    isPC = true;
-} else {
+if (window.screen.orientation.type == 'landscape-primary' && window.screen.width < 1024) {
     isPC = false;
+} else if (window.screen.orientation.type == 'portrait-primary' && window.screen.height < 1024) {
+    isPC = false;
+} else {
+    isPC = true;
 }
 
-isPC ? sectionTitle.concat(" tracking-normal") : sectionTitle.concat(" tracking-tighter");
+isPC 
+    ? sectionTitle = "font-Sacramento xl:text-8xl sm:text-7xl text-6xl font-extrabold mx-auto text-transparent pt-2 pb-5 px-8 bg-clip-text bg-gradient-to-r from-orange-500 to-purple-500"
+    : sectionTitle = "font-Sacramento xl:text-8xl sm:text-7xl text-6xl font-extrabold mx-auto text-transparent pt-2 pb-5 px-8 bg-clip-text bg-gradient-to-r from-orange-500 to-purple-500 tracking-tighter"
+
+console.log(sectionTitle)
 
 export {
     section,
