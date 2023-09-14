f972e891366a5466fb940473f5c390e6562b4929
alina-tariq <alina.tariq@ryerson.ca>
2023-09-13 22:00:01 -0400

    refactored common project card styles + added personal website to projects

---
+++ src/assets/js/projectsStyle.js
@@ -0,0 +1,20 @@
+// project cards
+const cardDiv = "md:w-60 w-52 rounded-lg shadow m-6";
+const imageDiv = "flex border-4 rounded-t-lg border-amber-500 items-center justify-center text-center";
+const image = "md:h-56 h-40 rounded-t-lg";
+const textDiv = "h-[16rem] flex flex-col p-3 border-x-4 rounded-b-lg border-b-4 border-amber-500 justify-center text-center";
+const title = "mb-2 md:text-xl text-lg font-bold tracking-tight text-white";
+const description = "mb-3 font-normal text-sm text-white";
+const tagDiv = "items-center justify-center px-2 pt-2";
+const tag = "inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2";
+
+export {
+    cardDiv,
+    imageDiv,
+    image,
+    textDiv,
+    title,
+    description,
+    tagDiv,
+    tag,
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import { section, sectionTitleDiv, sectionTitleUnderline, sectionTitle, contentDiv, content } from "../assets/js/standardStyle";
+import { cardDiv, imageDiv, image, textDiv, title, description, tagDiv, tag } from "../assets/js/projectsStyle";
 
 export default function Projects() {
     return (
@@ -18,75 +19,92 @@ export default function Projects() {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rutrum ex ligula, sed efficitur nulla lobortis quis. Duis pellentesque sit amet mi a laoreet. Proin quam lectus, ultricies eget massa et, gravida semper elit. Praesent pretium quam quam, in consectetur justo sollicitudin sit amet. 
                         </h3> */}
                         <div className="flex flex-wrap justify-center text-center px-2">
-                            <div class="md:w-60 w-52 rounded-lg shadow m-6">
-                                <div className="bg-white flex border-4 rounded-t-lg border-amber-500 items-center justify-center text-center">
+                            <div className={cardDiv}>
+                                <div className={"bg-white " + imageDiv}>
                                     <a href="https://github.com/alina-tariq/CrochetCompanion">
-                                        <img class="md:h-56 h-40 rounded-t-lg" src={require("../assets/img/crochetCompanion.png")} alt="" />
+                                        <img className={image} src={require("../assets/img/crochetCompanion.png")} alt="" />
                                     </a>
                                 </div>
-                                <div class="h-[16rem] flex flex-col px-3 py-4 border-x-4 rounded-b-lg border-b-4 border-amber-500 justify-center text-center">
+                                <div className={textDiv}>
                                     <a href="https://github.com/alina-tariq/CrochetCompanion">
-                                        <h5 class="mb-2 md:text-xl text-lg font-bold tracking-tight text-white">Crochet Companion</h5>
+                                        <h5 className={title}>Crochet Companion</h5>
                                     </a>
-                                    <p class="mb-3 font-normal text-sm text-white">An app designed to keep track of all things crochet.</p>
-                                    <div class="items-center justify-center px-2 pt-2">
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#swift</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#swiftui</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#realm</span>
+                                    <p className={description}>An app designed to keep track of all things crochet.</p>
+                                    <div className={tagDiv}>
+                                        <span className={tag}>#swift</span>
+                                        <span className={tag}>#swiftui</span>
+                                        <span className={tag}>#realm</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="md:w-60 w-52 rounded-lg shadow m-6">
-                                <div className="bg-[#282828] flex border-4 rounded-t-lg border-amber-500 items-center justify-center text-center">
+                            <div className={cardDiv}>
+                                <div className={"bg-[#282828] " + imageDiv}>
                                     <a href="https://github.com/alina-tariq/mastermind">
-                                        <img class="md:h-56 h-40 rounded-t-lg" src={require("../assets/img/mastermind.png")} alt="" />
+                                        <img className={image} src={require("../assets/img/mastermind.png")} alt="" />
                                     </a>
                                 </div>
-                                <div class="h-[16rem] flex flex-col px-3 py-4 border-x-4 rounded-b-lg border-b-4 border-amber-500 justify-center text-center">
+                                <div className={textDiv}>
                                     <a href="https://github.com/alina-tariq/mastermind">
-                                        <h5 class="mb-2 md:text-xl text-lg font-bold tracking-tight text-white">Mastermind</h5>
+                                        <h5 className={title}>Mastermind</h5>
                                     </a>
-                                    <p class="mb-3 font-normal text-sm text-white">A command line, multi-level version of the classic logic game Mastermind.</p>
-                                    <div class="items-center justify-center px-2 pt-2">
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#python</span>
+                                    <p className={description}>A command line, multi-level version of the classic logic game Mastermind.</p>
+                                    <div className={tagDiv}>
+                                        <span className={tag}>#python</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="md:w-60 w-52 rounded-lg shadow m-6">
-                                <div className="bg-[#4b674b] flex border-4 rounded-t-lg border-amber-500 items-center justify-center text-center">
+                            <div className={cardDiv}>
+                                <div className={"bg-[#4b674b] " + imageDiv}>
                                     <a href="https://github.com/alina-tariq/smartcustomerservices">
-                                        <img class="md:h-56 h-40 rounded-t-lg" src={require("../assets/img/scs.png")} alt="" />
+                                        <img className={image} src={require("../assets/img/scs.png")} alt="" />
                                     </a>
                                 </div>
-                                <div class="h-[16rem] flex flex-col px-3 py-4 border-x-4 rounded-b-lg border-b-4 border-amber-500 justify-center text-center">
+                                <div className={textDiv}>
                                     <a href="https://github.com/alina-tariq/smartcustomerservices">
-                                        <h5 class="mb-2 md:text-xl text-lg font-bold tracking-tight text-white">Smart Customer Services</h5>
+                                        <h5 className={title}>Smart Customer Services</h5>
                                     </a>
-                                    <p class="mb-3 font-normal text-sm text-white">A online grocery shopping website.</p>
-                                    <div class="items-center justify-center px-2 pt-2">
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#angularjs</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#html/css</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#php</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#sql</span>
+                                    <p className={description}>A online grocery shopping website.</p>
+                                    <div className={tagDiv}>
+                                        <span className={tag}>#angularjs</span>
+                                        <span className={tag}>#html/css</span>
+                                        <span className={tag}>#php</span>
+                                        <span className={tag}>#sql</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="md:w-60 w-52 rounded-lg shadow m-6">
-                                <div className="bg-[#b6a5b6] flex border-4 rounded-t-lg border-amber-500 items-center justify-center text-center">
+                            <div className={cardDiv}>
+                                <div className={"bg-[#b6a5b6] " + imageDiv}>
                                     <a href="https://github.com/alina-tariq/mindtrack">
-                                        <img class="md:h-56 h-40 rounded-t-lg" src={require("../assets/img/mindtrack.png")} alt="" />
+                                        <img className={image} src={require("../assets/img/mindtrack.png")} alt="" />
                                     </a>
                                 </div>
-                                <div class="h-[16rem] flex flex-col px-3 py-4 border-x-4 rounded-b-lg border-b-4 border-amber-500 justify-center text-center">
+                                <div className={textDiv}>
                                     <a href="https://github.com/alina-tariq/mindtrack">
-                                        <h5 class="mb-2 md:text-xl text-lg font-bold tracking-tight text-white">MindTrack</h5>
+                                        <h5 className={title}>MindTrack</h5>
                                     </a>
-                                    <p class="mb-3 font-normal text-sm text-white">A website to help you track your mental health.</p>
-                                    <div class="items-center justify-center px-2 pt-2">
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#html/css</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#javascript</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#php</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">#sql</span>
+                                    <p className={description}>A website to help you track your mental health.</p>
+                                    <div className={tagDiv}>
+                                        <span className={tag}>#html/css</span>
+                                        <span className={tag}>#javascript</span>
+                                        <span className={tag}>#php</span>
+                                        <span className={tag}>#sql</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={cardDiv}>
+                                <div className={"bg-[#156e9d] " + imageDiv}>
+                                    <a href="https://github.com/alina-tariq/personal-website">
+                                        <img className={image} src={require("../assets/img/website.png")} alt="" />
+                                    </a>
+                                </div>
+                                <div className={textDiv}>
+                                    <a href="https://github.com/alina-tariq/personal-website">
+                                        <h5 className={title}>Personal Website</h5>
+                                    </a>
+                                    <p className={description}>A website designed to display my portfolio.</p>
+                                    <div className={tagDiv}>
+                                        <span className={tag}>#react.js</span>
+                                        <span className={tag}>#tailwindcss</span>
                                     </div>
                                 </div>
                             </div>
